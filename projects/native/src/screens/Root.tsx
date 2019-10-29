5c8007aca08e576dcd848be574ddea70248866c1
Anton Vinogradov <winogradovaa@gmail.com>
2019-10-30 00:50:12 +0300

    feat(native): add Swing component

--- projects/native/src/screens/Root.tsx
+++ projects/native/src/screens/Root.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { View, Button, Text } from 'react-native';
 import styled from 'styled-components/native';
 
 import { userSignout } from '../providers/redux/actions/user';
+import { Swing } from '../components/Swing';
 import { TravelPlan } from '../components/TravelPlan';
 
 const StyledView = styled.View`
@@ -18,8 +19,13 @@ const mapDispatchProps = (dispatch: Dispatch) => bindActionCreators({ signout: u
 export type RootScreenProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchProps>;
 
 const Root: React.FC<RootScreenProps> = props => {
+  const [visible, setVisible] = useState(true);
+
   return (
     <StyledView>
+      <Swing visible={visible} onClose={() => setVisible(false)}>
+        <Text>http://instagram.com/</Text>
+      </Swing>
       <View style={{ paddingTop: 100 }}>
         <Text>Welcome {props.user.auth.email}</Text>
         <Button onPress={props.signout} title="Logout" />
