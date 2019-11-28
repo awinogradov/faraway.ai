360d30f43b86f102431f0d592cd0d337d60e7578
Anton Vinogradov <winogradovaa@gmail.com>
2019-11-28 18:28:31 +0300

    chore(functions): optimize parsers

--- projects/functions/src/index.ts
+++ projects/functions/src/index.ts
@@ -1,26 +1,15 @@
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
 
-import { resolveParseStrategy } from './parse';
-
-// const envinfo = require('envinfo');
-
-// envinfo.run(
-//   {
-//     System: ['OS', 'CPU', 'Memory', 'Shell'],
-//     Binaries: ['Node', 'Yarn', 'npm'],
-//     Databases: ['MongoDB'],
-//   },
-//   { json: true, console: true, showNotFound: true },
-// );
+import { scrapInstagram } from './providers/instagram';
+import { findOnGoogleMaps } from './providers/googleMaps';
 
 // https://firebase.google.com/docs/functions/typescript
 
 // export const parse = functions.https.onRequest(
 // response.json()
 export const parse = {
-  main: functions.https.onCall(async data => {
-    return resolveParseStrategy(data.url);
-  }),
+  instagram: https.onCall(scrapInstagram),
+  googleMaps: https.onCall(findOnGoogleMaps),
 };
 
 export * from './providers/database';
